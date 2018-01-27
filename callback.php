58d14820e1a66bf7fe31c25e8a32afe095b993fb
yoichi_gmf <yoichi.kayama@gmail.com>
2018-01-28 08:54:49 +0900

    modify logic

--- callback.php
+++ callback.php
@@ -1,37 +1,15 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-//POST
-$input = file_get_contents('php://input');
-$json = json_decode($input);
-$event = $json->events[0];
-//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CyQ8Ia7ukcEI9yct3NgYg8jzfyzN5hFQZx4Bq6TASr1jG6OKwQJcIdfV+ZWH0PSPbCVA6u1gM95paHuf6dKsXUikc+biVe/9rnCb+tN4wA6r1VDNqd1WNUinPgBzEX1AVA10eNojxFnOKTNJZABeqAdB04t89/1O/w1cDnyilFU=');
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LineMessageAPIChannelAccessToken'));
 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
-//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '129ff7d5adcce39f00a9c150d1593b40']);
-
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('LineMessageAPIChannelSecret')]);
-
-//イベントタイプ判別
-if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
-    //テキストメッセージにはオウムで返す
-    if ("text" == $event->message->type) {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
-    } else {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
-    }
-} elseif ("follow" == $event->type) {        //お友達追加時
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
-} elseif ("join" == $event->type) {           //グループに入ったときのイベント
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
-} elseif ('beacon' == $event->type) {         //Beaconイベント
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
-} else {
-    //なにもしない
+foreach ($events as $event) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
+      !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    continue;
+  }
+  $bot->replyText($event->getReplyToken(), $event->getText());
 }
-
-
-$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
-syslog(LOG_EMERG, print_r($event->replyToken, true));
-syslog(LOG_EMERG, print_r($response, true));
-return;
