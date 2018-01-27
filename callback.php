79e273ceaebd95a97141cff09d5b4f5c3d0e91ea
yoichi_gmf <yoichi.kayama@gmail.com>
2018-01-28 08:31:43 +0900

    change about token get from environment variable

--- callback.php
+++ callback.php
@@ -4,8 +4,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 $input = file_get_contents('php://input');
 $json = json_decode($input);
 $event = $json->events[0];
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CyQ8Ia7ukcEI9yct3NgYg8jzfyzN5hFQZx4Bq6TASr1jG6OKwQJcIdfV+ZWH0PSPbCVA6u1gM95paHuf6dKsXUikc+biVe/9rnCb+tN4wA6r1VDNqd1WNUinPgBzEX1AVA10eNojxFnOKTNJZABeqAdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '129ff7d5adcce39f00a9c150d1593b40']);
+//$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CyQ8Ia7ukcEI9yct3NgYg8jzfyzN5hFQZx4Bq6TASr1jG6OKwQJcIdfV+ZWH0PSPbCVA6u1gM95paHuf6dKsXUikc+biVe/9rnCb+tN4wA6r1VDNqd1WNUinPgBzEX1AVA10eNojxFnOKTNJZABeqAdB04t89/1O/w1cDnyilFU=');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+
+//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '129ff7d5adcce39f00a9c150d1593b40']);
+
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
 //イベントタイプ判別
 if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
     //テキストメッセージにはオウムで返す
