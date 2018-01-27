d6e10a499ce771f2f8b807bac81c07ab46988922
yoichi_gmf <yoichi.kayama@gmail.com>
2018-01-27 21:51:53 +0900

    add sdk

---
+++ callback.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+//POST
+$input = file_get_contents('php://input');
+$json = json_decode($input);
+$event = $json->events[0];
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('CyQ8Ia7ukcEI9yct3NgYg8jzfyzN5hFQZx4Bq6TASr1jG6OKwQJcIdfV+ZWH0PSPbCVA6u1gM95paHuf6dKsXUikc+biVe/9rnCb+tN4wA6r1VDNqd1WNUinPgBzEX1AVA10eNojxFnOKTNJZABeqAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '129ff7d5adcce39f00a9c150d1593b40']);
+//イベントタイプ判別
+if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
+    //テキストメッセージにはオウムで返す
+    if ("text" == $event->message->type) {
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    } else {
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
+    }
+} elseif ("follow" == $event->type) {        //お友達追加時
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
+} elseif ("join" == $event->type) {           //グループに入ったときのイベント
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
+} elseif ('beacon' == $event->type) {         //Beaconイベント
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
+} else {
+    //なにもしない
+}
+
+
+$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+syslog(LOG_EMERG, print_r($event->replyToken, true));
+syslog(LOG_EMERG, print_r($response, true));
+return;
