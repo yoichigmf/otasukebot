5c5db15c05daa984706d9ae3e88f4abcebb0423c
yoichi_gmf <yoichi.kayama@gmail.com>
2018-02-04 12:06:53 +0900

    add confirm

--- callback.php
+++ callback.php
@@ -9,6 +9,8 @@ $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
+$page = 1;
+
 foreach ($events as $event) {
 
    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
@@ -18,9 +20,27 @@ foreach ($events as $event) {
              continue;
       }
       
+      bot->replyText($event->getReplyToken(), $page);
+}
+      continue;
       
- 
   }
 
-  $bot->replyText($event->getReplyToken(), $event->getText());
+// 「はい」ボタン
+$yes_post = new PostbackTemplateActionBuilder("はい", "page={$page}");
+// 「いいえ」ボタン
+$no_post = new PostbackTemplateActionBuilder("いいえ", "page=-1");
+// Confirmテンプレートを作る
+$confirm = new ConfirmTemplateBuilder("メッセージ", [$yes_post, $no_post]);
+// Confirmメッセージを作る
+$confirm_message = new TemplateMessageBuilder("メッセージのタイトル", $confirm);
+
+$message = new MultiMessageBuilder();
+
+$message->add($confirm_message);
+// リプライTokenを付与して返信する
+$res = $bot->replyMessage($event->getReplyToken(), $message);
+
+
+  //$bot->replyText($event->getReplyToken(), $event->getText());
 }
