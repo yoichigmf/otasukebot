93eb7cfc0f94a83e23f38dfbdf087ebcdeefbb4c
yoichi_gmf <yoichi.kayama@gmail.com>
2018-02-04 12:00:00 +0900

    add condition

--- callback.php
+++ callback.php
@@ -2,15 +2,24 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('LineMessageAPIChannelAccessToken'));
+
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('LineMessageAPIChannelSecret')]);
+
 $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
 foreach ($events as $event) {
 
    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
       !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-    continue;
+      
+      if (!($event instanceof \LINE\LINEBot\Event\PostbackEvent) ) {
+             continue;
+      }
+      
+      
+ 
   }
 
   $bot->replyText($event->getReplyToken(), $event->getText());
