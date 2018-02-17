ab1622d205ef7502d6b9ff939d8fbe5dc465961d
yoichi_gmf <yoichi.kayama@gmail.com>
2018-02-17 12:37:53 +0900

    debug variable name

--- callback.php
+++ callback.php
@@ -81,7 +81,7 @@ foreach ($events as $event) {
 function  notsupport( $boti, $eventi, $targeti )
 {
 
-       $bot->replyText($eventi->getReplyToken(), $targeti);
+       $boti->replyText($eventi->getReplyToken(), $targeti);
        
        
 $actions = array(
