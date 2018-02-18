90e67d5949b2debf0e4cb47c38fb5b6e19f26a3f
yoichi <yoichi.kayama@gmail.com>
2018-02-18 19:02:49 +0900

    debug 1

--- callback.php
+++ callback.php
@@ -172,7 +172,7 @@ $res = $boti->replyMessage($eventi->getReplyToken(),$msg);
 function  hanteimenu( $boti, $eventi, $mnmsg, $tgscore )
 {
 
-    //    $boti->replyText($eventi->getReplyToken(), $targeti);
+    //      $boti->replyText($eventi->getReplyToken(), $targeti);
        
        
 $actions = array(
