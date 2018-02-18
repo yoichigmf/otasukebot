0a38f4dcde0c0777a9fa628e375619eff8114555
yoichi_gmf <yoichi.kayama@gmail.com>
2018-02-19 00:17:33 +0900

    separate  some menus menu.php

--- callback.php
+++ callback.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 require_once __DIR__ . '/vendor/autoload.php';
+require "menu.php"; //menu.phpのプログラムを使うよ
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -534,40 +535,7 @@ $res = $boti->replyMessage($eventi->getReplyToken(),$msg);
 
 
 
-function confirmmessage( $boti, $eventi, $pagen )
-{
 
-// 「はい」ボタン
-$yes_post = new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("はい", "action=select2&target={$pagen}");
-// 「いいえ」ボタン
-$no_post = new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("いいえ", "action=cancel");
-// Confirmテンプレートを作る
-$confirm = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder("親御さんについてのお困りごとですか?", [$yes_post, $no_post]);
-// Confirmメッセージを作る
-$confirm_message = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("メッセージのタイトル", $confirm);
-
-$message = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
-
-$message->add($confirm_message);
-// リプライTokenを付与して返信する
-$res = $boti->replyMessage($eventi->getReplyToken(), $message);
-}
-
-
-function firstmessage( $boti, $eventi, $pagen )
-{
-
-$actions = array(
-  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("認知症関係?", "action=select&menu=nextmenu&target=nintisyou"),
-  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("生活関係？", "action=select&target=seikatu"),
-    new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("教育関係？", "action=select&target=kyouiku"),
-       new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("障がい者福祉関係？", "action=select")
-);
- 
-$img_url = "https://otasukebot.herokuapp.com/otasuke.png";
-$button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("お悩み困りごとお助け","困りごとの種類は？", $img_url, $actions);
-$msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("困りごとの種類は？", $button);
-$res = $boti->replyMessage($eventi->getReplyToken(),$msg);
 
 
 
