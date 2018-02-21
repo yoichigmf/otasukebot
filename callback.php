8c649d0a308a9582c73622ca8da122a89212d008
yoichi_gmf <yoichi.kayama@gmail.com>
2018-02-22 00:18:16 +0900

    debug

--- callback.php
+++ callback.php
@@ -149,7 +149,7 @@ foreach ($events as $event) {
                   if ( strcmp($action, "browse" )==0  ) {    //  browse サービス・支援検索 
                   
                      // if ( $page > 1 ) {
-                   // $bot->replyText($event->getReplyToken(), $query);
+                    $bot->replyText($event->getReplyToken(), $query);
                   
                       //  }
                         
