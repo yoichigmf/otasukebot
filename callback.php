9b8f4e2f39bb952f90c869dd38c53c64bf4a9eec
yoichi_gmf <yoichi.kayama@gmail.com>
2018-02-24 06:36:52 +0900

    debug

--- callback.php
+++ callback.php
@@ -318,7 +318,7 @@ if ( count($response) > 0 ) {
                             $nn = $ncount + 1;
                   
                            $tgm1 = "自立度${jiritudo}向け ${kindi} サービス・支援検索 その${nn}";
-                           $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("自立度${jiritudo}", $tgm , $img_url, $actions);
+                           $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("自立度${jiritudo}", $tgm1 , $img_url, $actions);
                            $msgB = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("自立度${jiritudo} ${kindi}", $button); 
                             $multiplemsg->add( $msgB );
                            $ct = 0;
